4712a40700b89e363a31b3df44c723c6f7adb50b
Sutaai <sutaai@proton.me>
2026-02-22 01:20:19 +0100

    fix port

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,12 @@ variable "proxmox_base_fqdn_or_ip" {
   description = "The URL of the Proxmox's API."
 }
 
+variable "proxmox_port" {
+  type = string
+  description = "HTTPS port"
+  default = "8006"
+}
+
 variable "proxmox_user" {
   type        = string
   description = "The user to connect to Proxmox."
@@ -31,5 +37,5 @@ locals {
   beta_ip = element(element(proxmox_virtual_environment_vm.beta.ipv4_addresses, 1), 0)
   charlie_ip = element(element(proxmox_virtual_environment_vm.charlie.ipv4_addresses, 1), 0)
 
-  proxmox_url = "https://${var.proxmox_base_fqdn_or_ip}/api2/json"
+  proxmox_url = "https://${var.proxmox_base_fqdn_or_ip}:${var.proxmox_port}/api2/json"
 }
